3b0b1a6f45a2de42f1c422a167bb3aea2935d0c4
Pahanisshe <72974225+Pahanisshe@users.noreply.github.com>
2021-02-28 13:47:09 +0300

    task10

--- lab1/lab1.pl
+++ lab1/lab1.pl
@@ -71,3 +71,6 @@ grand_pas(X):-grand_pa(Y,X),write(Y),nl,fail.
 %task9
 grand_son(X,Y):-parent(Z,X),parent(Y,Z),man(X).
 grand_sons(X):-grand_son(Y,X),write(Y),nl,fail.
+
+%task10
+grand_pa_and_son(X,Y):-grand_pa(X,Y),grand_son(Y,X);grand_pa(Y,X),grand_son(X,Y).
