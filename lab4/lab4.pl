d9cba3da7e38c16e1cb392379c31f3729252cbc8
Pahanisshhe <72974225+Pahanisshhe@users.noreply.github.com>
2021-06-04 20:07:51 +0300

    Update lab4.pl

--- lab4/lab4.pl
+++ lab4/lab4.pl
@@ -1,12 +1,16 @@
 %чтение списка
 read_list(0,[]):-!.
-read_list(N,[H|T]):- read(H),N1 is N - 1, read_list(N1,T).
+read_list(N,[H|T]):-
+  read(H),N1 is N - 1, read_list(N1,T).
 
 %вывод списка
 write_list([]):-!.
-write_list([H|T]):- write(H), write(", "), write_list(T).
+write_list([H|T]):-
+  write(H), write(", "), write_list(T).
 
 %сумма элементов списка, рекурсия вниз
-sum_list_down(List,Sum):- sum_list_down(List,0,Sum).
+sum_list_down(List,Sum):-
+  sum_list_down(List,0,Sum).
 sum_list_down([],Sum,Sum):-!.
-sum_list_down([H|T],S,Sum):- S1 is S + H, sum_list_down(T,S1,Sum).
+sum_list_down([H|T],S,Sum):-
+  S1 is S + H, sum_list_down(T,S1,Sum).
