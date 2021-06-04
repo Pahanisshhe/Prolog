63f72a4ba4027c598f0bb470760d0d9565446c9f
Pahanisshhe <72974225+Pahanisshhe@users.noreply.github.com>
2021-06-04 20:06:40 +0300

    Update lab4.pl

--- lab4/lab4.pl
+++ lab4/lab4.pl
@@ -1,3 +1,7 @@
 %чтение списка
 read_list(0,[]):-!.
 read_list(N,[H|T]):- read(H),N1 is N - 1, read_list(N1,T).
+
+%вывод списка
+write_list([]):-!.
+write_list([H|T]):- write(H), write(", "), write_list(T).
